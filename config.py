66201c695a8a57d7981522f4df3ea9ee99367010
Outlook2530 <terabaap2530@outlook.com>
2025-09-18 21:47:15 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_NAME = os.path.join(BASE_DIR, "tasks.db")
+SECRET_KEY = "3a4f82d59c6e4f0a8e912a5d1f7c3b2e6f9a8d4c5b7e1d1a4c"
+ADMIN_PASSWORD = "AXSHU143"
